fbcc51ad02033b5565b2dad653e754b011c01cda
RobloxManFaceMug <nbahtnxohob@gmail.com>
2026-09-20 18:15:57 +0700

    blue

--- test_version.cpp
+++ test_version.cpp
@@ -7,12 +7,17 @@
 
 BOOST_AUTO_TEST_SUITE(test_sorting)
 
-BOOST_AUTO_TEST_CASE(test_valid_version)
+BOOST_AUTO_TEST_CASE(test_splitting)
 {
     BOOST_CHECK(DidSplit());
+}
+BOOST_AUTO_TEST_CASE(test_simple_sorting)
+{
     BOOST_CHECK(DidSort());
+}
+BOOST_AUTO_TEST_CASE(test_full_sorting)
+{
     BOOST_CHECK(DidSort2());
 }
-
 
 BOOST_AUTO_TEST_SUITE_END()
